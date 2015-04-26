3b0fbe4c07b470bf5b85bd150acb328af8704f65
Rohit Bhoompally <bhoompally@live.com>
2015-04-25 18:08:50 -0700

    Auto layout for row cell setup

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -22,11 +22,45 @@ class BusinessCell: UITableViewCell {
     var business: Business! {
         didSet {
             bnsNameLabel.text = business.name
+            
+            // Set Image with a fade-in effect
+            var urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: business.imageURL!)
+            bnsImgView.setImageWithURLRequest(urlRequest, placeholderImage: nil, success: { (request:NSURLRequest!, response:NSHTTPURLResponse!, image:UIImage!) -> Void in
+                if urlRequest != request {
+                    self.bnsImgView.image = image
+                } else {
+                    UIView.transitionWithView(self.bnsImgView, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
+                        self.bnsImgView.image = image
+                        }, completion: nil)
+                }
+                }) { (request:NSURLRequest!, response:NSHTTPURLResponse!, error:NSError!) -> Void in
+            }
+            
+            // Set other label properties
+            bnsAttributesLabel.text = business.categories
+            bnsAddressLabel.text = business.address
+            bnsDistanceLabel.text = business.distance
+            bnsNumberOfReviewsLabel.text = "\(business.reviewCount!) Reviews"
+            bnsRatingImgView.setImageWithURL(business.ratingImageURL)
         }
     }
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // Make the corners rounded
+        bnsImgView.layer.cornerRadius = 4
+        
+        // Clip the bitmap
+        bnsImgView.clipsToBounds = true
+        
+        // Fix the bug where the label doesn't wrap to next line
+        bnsNameLabel.preferredMaxLayoutWidth = bnsNameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Fix the bug where the label doesn't wrap to next line
+        bnsNameLabel.preferredMaxLayoutWidth = bnsNameLabel.frame.size.width
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
